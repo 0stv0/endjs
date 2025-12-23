bc8aea932a829985d8df57419980719d2de9ef20
stevku <75485384+0stv0@users.noreply.github.com>
2025-12-23 20:52:21 +0100

    1.0

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import { Server } from "./class/Server.js";
-
-const port: number = 3000;
-const sv: Server   = new Server(port);
-
-sv.addRoute("/", "GET", async(req, res) =>
-{
-    res.setStatus(200);
-    res.send({
-        message: 'test',
-        alkohol: 'denaturat'
-    });
-});
-
-sv.listen(() =>
-{
-    console.log(`API listening on http://localhost:${port}`);
-});
+export * from './class/Server.js';
+export * from './class/Route.js';
+export * from './lib/Mongo.js';
+export * from './lib/MySQL.js';
+export * from './lib/Passwords.js';
+export * from './lib/Tokens.js';
+export * from './lib/Types.js';
+export * from './types/EndRequest.js';
+export * from './types/EndResponse.js';
